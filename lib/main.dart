636640e149819ace5d58da04834842613f369283
YOHA <estif0919@gmail.com>
2026-04-08 17:13:31 +0300

    ii

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Mechu Coffee',
       theme: ThemeData(
       
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
